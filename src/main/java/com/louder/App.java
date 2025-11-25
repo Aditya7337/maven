c440e07d80ccc60e99157c30702f0b4705819502
Aditya Ranjan Padhy <113083591+Aditya7337@users.noreply.github.com>
2025-11-25 15:54:31 +0530

    Update App.java

--- src/main/java/com/louder/App.java
+++ src/main/java/com/louder/App.java
@@ -8,9 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldddd !" );
+        System.out.println( "Hello World 1 !" );
     }
 }
+
 
 
 
